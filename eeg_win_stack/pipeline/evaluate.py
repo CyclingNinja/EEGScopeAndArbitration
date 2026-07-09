1c2228b58dd3a3301eed6e1cfab565030ee5092a
CyclingNinja <sam.m.bennett@proton.me>
2026-07-09 16:50:44 +0100

    End to end dvc run

--- eeg_win_stack/pipeline/evaluate.py
+++ eeg_win_stack/pipeline/evaluate.py
@@ -98,6 +98,8 @@ def main():
             }
         )
         mlflow.log_metrics(metrics)
+        latest_pt = max(Path('target/saved_models').glob('*.pt'), key=lambda p: p.stat().st_mtime)
+        mlflow.log_artifact(str(latest_pt), artifact_path="model")
 
 
 if __name__ == "__main__":
